a62b3c9d6afa7ab98e9ee6c6c13e6b64702bfad3
Mark Lyen <mark@marklyen.com>
2017-06-08 15:27:23 -0700

    Add project files.

---
+++ TupleSandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TupleSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
